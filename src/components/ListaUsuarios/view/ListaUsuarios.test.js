c2549b74f963284d3046b99411c2886698331590
Rodrigo Vidal <rodrigovidal2006@gmail.com>
2019-11-17 18:03:49 -0300

    Primeiro teste OK

---
+++ src/components/ListaUsuarios/view/ListaUsuarios.test.js
@@ -0,0 +1,27 @@
+const faker = require('faker');
+const puppeteer = require('puppeteer');
+
+describe('Testa carregamento da página', () => {
+    test('Teste da exibição dos elementos', async () => {
+        let browser = await puppeteer.launch({
+            headless: false
+        });
+        let page = await browser.newPage();
+
+        page.emulate({
+            viewport: {
+                width: 500,
+                height: 2400
+            },
+            userAgent: ''
+        });
+
+        await page.goto('http://localhost:3000/');
+
+        const muiGridItem  = await page.$eval('.MuiGrid-item ', el => !!el);
+        const listItems = await page.$$('.MuiGrid-item');
+        expect(muiGridItem).toBe(true);
+        expect(listItems.length).toBe(6);
+        browser.close();
+    }, 16000);
+});
