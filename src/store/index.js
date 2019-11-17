058bfd4845cef24fc5e95d6d5c98542489ac29bc
Rodrigo Vidal <rodrigovidal2006@gmail.com>
2019-11-17 17:00:11 -0300

    Dev tools somente no modo development

--- src/store/index.js
+++ src/store/index.js
@@ -3,18 +3,13 @@ import {applyMiddleware, createStore, compose} from 'redux';
 import {mainSaga} from "./mainSaga";
 import createSagaMiddleware from 'redux-saga';
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__;
-
-/*if (process.env.NODE_ENV === 'development') {
-    const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
-
-    if (typeof devToolsExtension === 'function') {
-        enhancers.push(devToolsExtension())
-    }
-}*/
 const enhancers = [];
-if (typeof devTools === 'function') {
-    enhancers.push(devTools())
+
+if (process.env.NODE_ENV === 'development') {
+    const devTools = window.__REDUX_DEVTOOLS_EXTENSION__;
+    if (typeof devTools === 'function') {
+        enhancers.push(devTools())
+    }
 }
 
 const sagaMiddleware = createSagaMiddleware();
