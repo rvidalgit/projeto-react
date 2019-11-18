220e1f12d9fe761c68c65bb564a7b5cab4f4d0f0
Rodrigo Vidal <rodrigovidal2006@gmail.com>
2019-11-18 08:29:17 -0300

    Novo teste
    Atualização dos testes

--- src/components/ListaUsuarios/view/ListaUsuarios.test.js
+++ src/components/ListaUsuarios/view/ListaUsuarios.test.js
@@ -10,8 +10,8 @@ describe('Testa carregamento da página', () => {
 
         page.emulate({
             viewport: {
-                width: 500,
-                height: 2400
+                width: document.documentElement.clientWidth,
+                height: document.documentElement.clientHeight
             },
             userAgent: ''
         });
@@ -22,6 +22,28 @@ describe('Testa carregamento da página', () => {
         const listItems = await page.$$('.MuiGrid-item');
         expect(muiGridItem).toBe(true);
         expect(listItems.length).toBe(6);
+        browser.close();
+    }, 16000);
+});
+
+describe('Screenshot do mobile', () => {
+    test('Captura a exibição dos itens no celular', async () => {
+        let browser = await puppeteer.launch({
+            headless: false
+        });
+        let page = await browser.newPage();
+
+        page.emulate({
+            viewport: {
+                width: 440,
+                height: 600
+            },
+            userAgent: ''
+        });
+        await page.goto('http://localhost:3000/',{ waitUntil: 'networkidle0' });
+        const now = new Date;
+        const name = now.getFullYear().toString() + '-' + now.getMonth().toString() + '-' + now.getDate().toString();
+        await page.screenshot({path: `${name}-mobile.png`});
         browser.close();
     }, 16000);
 });
